d72a20c8f906d48c826b20cb2eccb8544ebeb152
omlan99 <95955202+omlan99@users.noreply.github.com>
2023-08-29 01:17:30 +0600

    code

---
+++ script.js
@@ -0,0 +1,9 @@
+let password = document.getElementById("pass");
+document.getElementById("eye").addEventListener('click', function(){
+    if(password.type === "password"){
+        password.type ==="text";
+    }
+    else{
+        password.type ==="password";
+    }
+})
